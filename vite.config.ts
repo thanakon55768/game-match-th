f7b4f5ddc11e83fe572d6bedfcadb88697a6847a
thanakon55768 <thanakorn55768@gmail.com>
2025-03-16 22:27:39 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // 🛠️ แก้ให้ base อยู่ที่ root
   server: {
-    host: "::",
+    host: "0.0.0.0", // หรือไม่ต้องกำหนด
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // ตรวจสอบว่าไฟล์ Build ถูกสร้างที่นี่
+    emptyOutDir: true,
   },
 }));
